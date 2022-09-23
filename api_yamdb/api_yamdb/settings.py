b828c16ad9acb6513573525509145a196211b605
Алексей Маринин <marinin.aleksej@yandex.ru>
2022-09-23 22:08:38 +0300

    Test1

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -65,10 +65,10 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
-        'USER': os.getenv('POSTGRES_USER', default='ptDFSDF'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='SDFNSNSFD'),
-        'HOST': os.getenv('DB_HOST', default='ASDFAS'),
-        'PORT': os.getenv('DB_PORT', default='5458')
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='Paj3tN537'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
 
